619e7231af9a3d781eef7af4750763a67152dcf4
CodeBuoi123 <vinhautomotive@gmail.com>
2021-06-20 10:11:44 +0700

    version2_vietChucNangRenderProduct

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,29 @@
 console.log ('hello cybersoft');
+
+
+var arrProduct = [
+    {id: 1,name: 'iphone X', price: 1000, img: 'https://unsplash.com/photos/pduutGbL2-M'},
+    {id: 2,name: 'iphone Xs', price: 2000, img: 'https://unsplash.com/photos/pduutGbL2-M'},
+    {id: 3,name: 'iphone Xs Max', price: 3000, img: 'https://unsplash.com/photos/pduutGbL2-M'}
+
+]
+
+function renderproduct () {
+    var content = '';
+    for (var i=0; i < arrProduct.length; i++) {
+        let product = arrProduct[i];
+        content += `
+            <div class="col-4 mt-2">
+                <div class="card">
+                    <img src="${product.img}" />
+                    <div class="card-body">
+                        <p>${product.price}</p>
+                        <button>Mua Hàng</button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+    document.querySelector('#arrProduct').innerHTML = content;
+}
+renderproduct();
